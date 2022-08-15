8085c5843a02b0db8924d58a2c388d54dfce9bcb
wangdongxv <1085240445@qq.com>
2022-08-15 22:18:30 +0800

    完成移除重复节点

--- src/程序员面试金典/10. 移除重复节点.ts
+++ src/程序员面试金典/10. 移除重复节点.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: 王东旭
  * @Date: 2022-08-06 22:38:53
- * @LastEditTime: 2022-08-07 22:39:42
+ * @LastEditTime: 2022-08-15 22:18:00
  * @LastEditors: 王东旭
  * @Description:
  * @FilePath: \letcode\src\程序员面试金典\10. 移除重复节点.ts
@@ -20,30 +20,29 @@
  */
 import { SingleLink, SingleNode } from "../links/SingleLink";
 
+
 function removeDuplicateNodes(head: SingleNode | null): SingleNode | null {
-  if (!head || !head.next) return head;
-  let map = new Map();
-  let cur = head;
-  while (cur) {
-    if (!map.has(cur.data)) {
-        cur.next = null
-      map.set(cur.data, cur);
+   if(!head || !head.next) return head;
+   let cur = head
+   let pre = head
+   const set = new Set()
+  while(cur){
+    if (set.has(cur.data)) {
+     pre.next = cur.next
+     cur = cur.next 
+    }else{
+      set.add(cur.data)
+      pre = cur
+      cur = cur.next
+
     }
-    cur = cur.next;
-  }
-  //   console.log(map);
 
-  let pre = head;
-  let tail = pre;
-  map.forEach((value) => {
-    tail.next = value;
-    tail = tail.next;
-  });
-
-  return pre.next;
+  } 
+  return head
 }
-
 let temp = new SingleLink([1,2,3,3,2,1]);
+console.log(temp.head);
+
 
 console.log(removeDuplicateNodes(temp.head));
 
