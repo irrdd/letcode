547208454a55f4a5aa87f874a0da31cdf0cb3d4f
wangdongxv <1085240445@qq.com>
2022-08-17 22:30:23 +0800

    跳过18题

---
+++ src/程序员面试金典/18.一个数组实现三个栈.ts
@@ -0,0 +1,43 @@
+/*
+ * @Author: 王东旭
+ * @Date: 2022-08-17 21:59:29
+ * @LastEditTime: 2022-08-17 22:19:51
+ * @LastEditors: 王东旭
+ * @Description: 
+ * @FilePath: \letcode\src\程序员面试金典\18.一个数组实现三个栈.ts
+ *  
+ */
+export class TripleInOne {
+    private $arr:number[]
+    constructor(stackSize: number) {
+
+    }
+
+    push(stackNum: number, value: number): void {
+
+    }
+
+    pop(stackNum: number): number {
+
+    }
+
+    peek(stackNum: number): number {
+
+    }
+
+    isEmpty(stackNum: number): boolean {
+
+    }
+    get arr(){
+        return this.$arr
+    }
+}
+
+/**
+ * Your TripleInOne object will be instantiated and called as such:
+ * var obj = new TripleInOne(stackSize)
+ * obj.push(stackNum,value)
+ * var param_2 = obj.pop(stackNum)
+ * var param_3 = obj.peek(stackNum)
+ * var param_4 = obj.isEmpty(stackNum)
+ */
