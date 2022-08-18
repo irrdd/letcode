3c49c9e94eb9b23def4b31cb73b6ba25a89828cc
wangdongxu <1085240445@qq.com>
2022-08-18 11:35:21 +0800

    配置测试的相对路径

--- jest.config.js
+++ jest.config.js
@@ -88,7 +88,11 @@ module.exports = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^#/(.*)$": "<rootDir>/src/程序员面试金典/$1",
+    "^%/(.*)$": "<rootDir>/src/剑指office/$1"
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
--- test/程序员面试金典/15.回文链表.spec.ts
+++ test/程序员面试金典/15.回文链表.spec.ts
@@ -1,16 +1,16 @@
 /*
  * @Author: 王东旭
  * @Date: 2022-08-17 09:48:27
- * @LastEditTime: 2022-08-17 10:15:09
+ * @LastEditTime: 2022-08-18 11:21:38
  * @LastEditors: 王东旭
  * @Description: 
  * @FilePath: /letcode/test/程序员面试金典/15.回文链表.spec.ts
  *  
  */
 
-import { SingleLink, ListNode } from "../../src/links/SingleLink";
-import { linkToArray } from "../../src/util/linkUtil";
-import {  isPalindrome} from "../../src/程序员面试金典/15.回文链表";
+import { SingleLink, ListNode } from "@/links/SingleLink";
+import { linkToArray } from "@/util/linkUtil";
+import {  isPalindrome} from "#/15.回文链表";
 
 function base(arr:number[],fn=isPalindrome):boolean{
     const temp = new SingleLink(arr)
