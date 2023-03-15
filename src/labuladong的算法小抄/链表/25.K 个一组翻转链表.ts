2357302b10586436f2959a782cde52255a3cf72a
wangdongxv <1085240445@qq.com>
2023-03-15 16:18:00 +0800

    feat: 25.K 个一组翻转链表

---
+++ src/labuladong的算法小抄/链表/25.K 个一组翻转链表.ts
@@ -0,0 +1,44 @@
+import { ListNode } from "@/links/SingleLink";
+import { reverseList } from "@/labuladong的算法小抄/链表/206.反转链表";
+import { reverseN } from '@/labuladong的算法小抄/链表/反转链表前N个节点'
+export function reverseKGroup(head: ListNode | null, k: number): ListNode | null {
+    if (!head || !head.next || k === 1) return head
+    let num = k - 1
+    const resHead = reverseN(head, k)
+    let temp = resHead
+    while (temp) {
+        if (num === 0) {
+            let tail = temp
+            let m = k
+            while (tail && m) {
+                tail = tail.next
+                m--
+            }
+            if (!tail) break;
+            temp.next = reverseN(temp.next, k)
+            num = k
+        } else {
+            num--
+            temp = temp.next
+        }
+    }
+
+    return resHead
+
+};
+
+export function reverseKGroup2(head: ListNode | null, k: number): ListNode | null {
+    if(!head || !head.next) return head
+    let tail = head
+    let num = k-1
+    while (tail && num) {
+       tail = tail.next 
+       num--
+    }
+    if (!tail) return head
+    const temp = reverseKGroup2(tail.next, k)
+    tail.next = null
+    const res = reverseList(head)
+    head.next = temp
+    return res
+};
---
+++ test/labuladong的算法小抄/链表/25.K 个一组翻转链表.spec.ts
@@ -0,0 +1,106 @@
+import { ListNode, SingleLink } from "@/links/SingleLink";
+import { reverseKGroup,reverseKGroup2 } from '@/labuladong的算法小抄/链表/25.K 个一组翻转链表'
+import { linkToArray } from "@/util/linkUtil";
+
+describe('普通方法', () => {
+    test('正常链表k组反转,k=3', () => {
+        const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+        const k = 3
+        const head = new SingleLink(arr).head
+        const resLink = reverseKGroup(head, k)
+        const resArr = linkToArray(resLink)
+        expect(resArr).toEqual([3, 2, 1, 6, 5, 4, 9, 8, 7, 0])
+    })
+
+    test('正常链表k组反转,k=2', () => {
+        const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+        const k = 2
+        const head = new SingleLink(arr).head
+        const resLink = reverseKGroup(head, k)
+        const resArr = linkToArray(resLink)
+        expect(resArr).toEqual([2, 1, 4, 3, 6, 5, 8, 7, 0, 9])
+    })
+    test('正常链表k组反转,k等于链表长度', () => {
+        const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+        const k = 10
+        const head = new SingleLink(arr).head
+        const resLink = reverseKGroup(head, k)
+        const resArr = linkToArray(resLink)
+        expect(resArr).toEqual([0, 9, 8, 7, 6, 5, 4, 3, 2, 1])
+    })
+    test('正常链表,k=1', () => {
+        const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+        const k = 1
+        const head = new SingleLink(arr).head
+        const resLink = reverseKGroup(head, k)
+        const resArr = linkToArray(resLink)
+        expect(resArr).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+    })
+    test('只有两个节点的链表,k=2', () => {
+        const arr = [1,2]
+        const k = 2 
+        const head = new SingleLink(arr).head
+        const resLink = reverseKGroup(head, k)
+        const resArr = linkToArray(resLink)
+        expect(resArr).toEqual([2,1])
+    })
+    test('只有一个头节点,k=1', () => {
+        const arr = [1]
+        const k = 1
+        const head = new SingleLink(arr).head
+        const resLink = reverseKGroup(head, k)
+        const resArr = linkToArray(resLink)
+        expect(resArr).toEqual([1])
+    })
+})
+describe('迭代', () => {
+    test('正常链表k组反转,k=3', () => {
+        const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+        const k = 3
+        const head = new SingleLink(arr).head
+        const resLink = reverseKGroup2(head, k)
+        const resArr = linkToArray(resLink)
+        expect(resArr).toEqual([3, 2, 1, 6, 5, 4, 9, 8, 7, 0])
+    })
+
+    test('正常链表k组反转,k=2', () => {
+        const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+        const k = 2
+        const head = new SingleLink(arr).head
+        const resLink = reverseKGroup2(head, k)
+        const resArr = linkToArray(resLink)
+        expect(resArr).toEqual([2, 1, 4, 3, 6, 5, 8, 7, 0, 9])
+    })
+    test('正常链表k组反转,k等于链表长度', () => {
+        const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+        const k = 10
+        const head = new SingleLink(arr).head
+        const resLink = reverseKGroup2(head, k)
+        const resArr = linkToArray(resLink)
+        expect(resArr).toEqual([0, 9, 8, 7, 6, 5, 4, 3, 2, 1])
+    })
+    test('正常链表,k=1', () => {
+        const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+        const k = 1
+        const head = new SingleLink(arr).head
+        const resLink = reverseKGroup2(head, k)
+        const resArr = linkToArray(resLink)
+        expect(resArr).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+    })
+    test('只有两个节点的链表,k=2', () => {
+        const arr = [1,2]
+        const k = 2 
+        const head = new SingleLink(arr).head
+        const resLink = reverseKGroup2(head, k)
+        const resArr = linkToArray(resLink)
+        expect(resArr).toEqual([2,1])
+    })
+    test('只有一个头节点,k=1', () => {
+        const arr = [1]
+        const k = 1
+        const head = new SingleLink(arr).head
+        const resLink = reverseKGroup2(head, k)
+        const resArr = linkToArray(resLink)
+        expect(resArr).toEqual([1])
+    })
+})
