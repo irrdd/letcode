300edcc43afbee36bceceb0454fd92ae78cdbe64
wangdongxv <1085240445@qq.com>
2023-04-07 11:30:59 +0800

    feat: 完成螺旋矩阵

--- src/labuladong的算法小抄/数组/54.螺旋矩阵.ts
+++ src/labuladong的算法小抄/数组/54.螺旋矩阵.ts
@@ -6,25 +6,25 @@ export function spiralOrder(matrix: number[][]): number[] {
     let left_bottom = 0
     let right_bottom = matrix.length -1
     while (arr.length < matrix.length * matrix[0].length) {
-        if (left_top < right_bottom) {
+        if (left_top <= right_bottom) {
             for (let index = left_bottom; index <= right_top; index++) {
                 arr.push(matrix[left_top][index])
             }
             left_top++
         }
-        if (left_bottom < right_top) {
-            for (let index = right_top; index <= right_bottom; index++) {
+        if (right_top >= left_bottom) {
+            for (let index = left_top; index <= right_bottom; index++) {
                 arr.push(matrix[index][right_top])
             }
             right_top--
         }
-        if (right_bottom > left_top) {
+        if (right_bottom >= left_top) {
             for (let index = right_top; index >= left_bottom; index--) {
                 arr.push(matrix[right_bottom][index])
             }
             right_bottom--
         }
-        if (left_bottom < right_top) {
+        if (left_bottom <= right_top) {
             for (let index = right_bottom; index >= left_top; index--) {
                arr.push(matrix[index][left_bottom]) 
             }
@@ -34,4 +34,4 @@ export function spiralOrder(matrix: number[][]): number[] {
 
     return arr
 
-};
+};  
