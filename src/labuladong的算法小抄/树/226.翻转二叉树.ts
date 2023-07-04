7f9c60613553d1f4fab9094df8d6e5c970fd3e29
wangdongxv <1085240445@qq.com>
2023-07-04 03:44:55 +0000

    增加一种解法

--- src/labuladong的算法小抄/树/226.翻转二叉树.ts
+++ src/labuladong的算法小抄/树/226.翻转二叉树.ts
@@ -1,8 +1,23 @@
+// 分解问题
 export function invertTree(root: TreeNode | null): TreeNode | null {
     if (!root) return null
     const right = invertTree(root.right)
     const left = invertTree(root.left)
     root.right = left
     root.left = right
+    return root
+};
+
+// 遍历
+export function invertTree2(root: TreeNode | null): TreeNode | null {
+    const traverse = (root: TreeNode | null) => {
+        if (!root) return null
+        traverse(root.left)
+        traverse(root.right)
+        const temp = root.left
+        root.left = root.right
+        root.right = temp
+    }
+    traverse(root)
     return root
 };
--- test/labuladong的算法小抄/树/226.翻转二叉树.spec.ts
+++ test/labuladong的算法小抄/树/226.翻转二叉树.spec.ts
@@ -1,6 +1,5 @@
 import { obj2Tree } from '@/util/treeNodeUtil'
-import { invertTree } from '@/labuladong的算法小抄/树/226.翻转二叉树'
-
+import { invertTree, invertTree2 } from '@/labuladong的算法小抄/树/226.翻转二叉树'
 
 describe('分解问题', () => {
 
@@ -148,6 +147,157 @@ describe('分解问题', () => {
         const tree = null;
 
         const invertedTree = invertTree(tree);
+
+        expect(invertedTree).toEqual(null);
+    });
+
+})
+describe('遍历', () => {
+
+    test('Example Test Case 1', () => {
+        const tree = {
+            val: 4,
+            left: {
+                val: 2,
+                left: {
+                    val: 1,
+                    left: null,
+                    right: null,
+                },
+                right: {
+                    val: 3,
+                    left: null,
+                    right: null,
+                },
+            },
+            right: {
+                val: 7,
+                left: {
+                    val: 6,
+                    left: null,
+                    right: null,
+                },
+                right: {
+                    val: 9,
+                    left: null,
+                    right: null,
+                },
+            },
+        };
+
+        const invertedTree = invertTree2(tree);
+
+        expect(invertedTree).toEqual({
+            val: 4,
+            left: {
+                val: 7,
+                left: {
+                    val: 9,
+                    left: null,
+                    right: null,
+                },
+                right: {
+                    val: 6,
+                    left: null,
+                    right: null,
+                },
+            },
+            right: {
+                val: 2,
+                left: {
+                    val: 3,
+                    left: null,
+                    right: null,
+                },
+                right: {
+                    val: 1,
+                    left: null,
+                    right: null,
+                },
+            },
+        });
+    });
+    test('Tree with Only Right Subtree', () => {
+        const tree = {
+            val: 1,
+            left: null,
+            right: {
+                val: 2,
+                left: null,
+                right: {
+                    val: 3,
+                    left: null,
+                    right: null,
+                },
+            },
+        };
+
+        const invertedTree = invertTree2(tree);
+
+        expect(invertedTree).toEqual({
+            val: 1,
+            left: {
+                val: 2,
+                left: {
+                    val: 3,
+                    left: null,
+                    right: null,
+                },
+                right: null,
+            },
+            right: null,
+        });
+    });
+    test('Tree with Only Left Subtree', () => {
+        const tree = {
+            val: 1,
+            left: {
+                val: 2,
+                left: {
+                    val: 3,
+                    left: null,
+                    right: null,
+                },
+                right: null,
+            },
+            right: null,
+        };
+
+        const invertedTree = invertTree2(tree);
+
+        expect(invertedTree).toEqual({
+            val: 1,
+            left: null,
+            right: {
+                val: 2,
+                left: null,
+                right: {
+                    val: 3,
+                    left: null,
+                    right: null,
+                },
+            },
+        });
+    });
+    test('Tree with Only Root Node', () => {
+        const tree = {
+            val: 1,
+            left: null,
+            right: null,
+        };
+
+        const invertedTree = invertTree2(tree);
+
+        expect(invertedTree).toEqual({
+            val: 1,
+            left: null,
+            right: null,
+        });
+    });
+    test('Empty Tree', () => {
+        const tree = null;
+
+        const invertedTree = invertTree2(tree);
 
         expect(invertedTree).toEqual(null);
     });
